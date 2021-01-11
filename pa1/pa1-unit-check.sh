5490f2d77747fe8954c52a9a6243c2e3574a27ef
Hou-I Lin <hlin64@ucsc.edu>
2021-01-11 11:47:49 -0800

    pa1 upload

--- pa1/pa1-unit-check.sh
+++ pa1/pa1-unit-check.sh
@@ -23,13 +23,13 @@ echo ""
 gcc -c -std=c11 -Wall -g ModelListTest.c List.c
 gcc -o ModelListTest ModelListTest.o List.o
 
-timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt &> ListTest-valgrind-out.txt
+timeout 5 valgrind --leak-check=full -v ./ModelListTest -v > ListTest-out.txt 2> ListTest-mem.txt
 
 cat ListTest-out.txt
 
 echo "List Valgrind Test: (press enter)"
 read garbage
-cat ListTest-valgrind-out.txt
+cat ListTest-mem.txt
 
-rm -f *.o ModelListTest* Lex ListTest-out.txt ListTest-valgrind-out.txt
+rm -f *.o ModelListTest* Lex ListTest-out.txt ListTest-mem.txt
 
